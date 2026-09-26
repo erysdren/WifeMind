05db10bc3ad8422528064df5da4374d5609b1987
erysdren <contact@erysdren.me>
2026-09-26 07:44:45 -0500

    cmake: update how revision is handled

--- cmake/fte_revision.cmake
+++ cmake/fte_revision.cmake
@@ -1,47 +1,55 @@
 
-if(EXISTS ${PROJECT_SOURCE_DIR}/.git AND NOT DEFINED FTE_REVISON)
-	execute_process(COMMAND
-		git describe --always --long --dirty
+find_package(Git)
+
+if(Git_FOUND AND EXISTS ${PROJECT_SOURCE_DIR}/.git)
+	execute_process(
+		COMMAND ${GIT_EXECUTABLE} describe --always --long --dirty
 		WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
-		OUTPUT_VARIABLE FTE_REVISON_GIT
-		ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+		OUTPUT_VARIABLE git_commit
+		ERROR_QUIET
+		OUTPUT_STRIP_TRAILING_WHITESPACE
 	)
-	execute_process(COMMAND
-		git log -1 --format=%cs
+	execute_process(
+		COMMAND ${GIT_EXECUTABLE} log -1 --format=%cs
 		WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
-		OUTPUT_VARIABLE FTE_DATE
-		ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+		OUTPUT_VARIABLE git_date
+		ERROR_QUIET
+		OUTPUT_STRIP_TRAILING_WHITESPACE
 	)
-	execute_process(COMMAND
-		git branch --show-current
+	execute_process(
+		COMMAND ${GIT_EXECUTABLE} branch --show-current
 		WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
-		OUTPUT_VARIABLE FTE_BRANCH
-		ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+		OUTPUT_VARIABLE git_branch
+		ERROR_QUIET
+		OUTPUT_STRIP_TRAILING_WHITESPACE
 	)
-	execute_process(COMMAND
-		git rev-parse --is-shallow-repository
+	execute_process(
+		COMMAND ${GIT_EXECUTABLE} rev-parse --is-shallow-repository
 		WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
-		OUTPUT_VARIABLE FTE_GIT_IS_SHALLOW
-		ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+		OUTPUT_VARIABLE git_is_shallow
+		ERROR_QUIET
+		OUTPUT_STRIP_TRAILING_WHITESPACE
 	)
-	if(FTE_GIT_IS_SHALLOW STREQUAL true)
+	if(git_is_shallow)
 		message(STATUS "shallow clone prevents calculation of revision number.")
-		set(SVNREVISION "git-${FTE_REVISON_GIT}")	#if its a shallow clone then we can't count commits properly so don't know what revision we actually are.
+		set(git_revision "git-${git_commit}") #if its a shallow clone then we can't count commits properly so don't know what revision we actually are.
 	else()
-		execute_process(COMMAND
-			git rev-list HEAD --count
+		execute_process(
+			COMMAND ${GIT_EXECUTABLE} rev-list HEAD --count
 			WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
-			OUTPUT_VARIABLE SVNREVISION
-			ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+			OUTPUT_VARIABLE git_revision
+			ERROR_QUIET
+			OUTPUT_STRIP_TRAILING_WHITESPACE
 		)
-		math(EXPR SVNREVISION "${SVNREVISION} + 29") #not all svn commits managed to appear im the git repo, so we have a small bias to keep things consistent.
-		if(FTE_BRANCH STREQUAL "master" OR FTE_BRANCH STREQUAL "")
-			set(SVNREVISION "${SVNREVISION}-git-${FTE_REVISON_GIT}")
+		math(EXPR git_revision "${git_revision} + 29") #not all svn commits managed to appear im the git repo, so we have a small bias to keep things consistent.
+		if(git_branch STREQUAL "main" OR git_branch STREQUAL "")
+			set(git_revision "${git_revision}-git-${git_commit}")
 		else()
-			set(SVNREVISION "${FTE_BRANCH}-${SVNREVISION}-git-${FTE_REVISON_GIT}") #weird branches get a different form of revision, to reduce confusion.
+			set(git_revision "${git_branch}-${git_revision}-git-${git_commit}") #weird branches get a different form of revision, to reduce confusion.
 		endif()
 	endif()
-	message(STATUS "FTE GIT ${FTE_BRANCH} Revision ${SVNREVISION}, ${FTE_DATE}")
-	set(FTE_REVISON SVNREVISION=${SVNREVISION} SVNDATE=${FTE_DATE} FTE_BRANCH=${FTE_BRANCH})
+	message(STATUS "FTE GIT ${git_branch} Revision ${git_revision}, ${git_date}")
+	list(APPEND FTE_COMMON_DEFINITIONS SVNREVISION=${git_revision})
+	list(APPEND FTE_COMMON_DEFINITIONS SVNDATE=${git_date})
+	list(APPEND FTE_COMMON_DEFINITIONS FTE_BRANCH=${git_branch})
 endif()
-
